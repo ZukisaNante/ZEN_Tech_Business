d15d9a8e82f87938cdc9d5dff3b5c1c3a864ab4d
junior <zukisanante@gmail.com>
2019-06-06 15:31:58 +0200

    if else is finished and it works accordingly

--- js/open.js
+++ js/open.js
@@ -1,21 +1,69 @@
 // verifying opening hours
-function dayNow() {
+/*function dayNow() {
     var greeting;
     var day = new Date();
     var time = new Date().getHours();
-    var n = day.toDateString();
-    document.getElementById("status_two").innerHTML = n;
-    if (time < 10) {
-      greeting = "Good morning";
+    var now = day.toDateString();
+    if (time < 12)  {
+      greeting = "Good morning" + status;
     } else if (time < 20) {
       greeting = "Good day";
     } else {
       greeting = "Good evening";
     }
-    document.getElementById("status_two").innerHTML = greeting;
+    document.getElementById("status_four").innerHTML = greeting;
 
-    if (time > 12){
+    if (time > 23){
         greeting = 'Closed!';
     }
-    document.getElementById("status_two").innerHTML = greeting;
+    document.getElementById("status_four").innerHTML = greeting;
+}*/
+function dayNow() {
+var greeting;
+var day = new Date();
+var now = day.toDateString();
+var time = new Date().getHours();
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+//document.getElementById("status_four").innerHTML = "Today is " + day;
+if (time < 12 || now == true)  {
+  greeting = "Good morning";
+} else if (time < 20 || now == true) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+
+if (time > 20 || now == true){
+    greeting = 'Closed!';}
+
+
+check();
+function check(){
+if (time>09 && time<20){
+document.getElementById("status_four").innerHTML = greeting + " today is " + day + " and we are still open! ";}
+else{
+  document.getElementById("status_four").innerHTML = greeting + " today is " + day + " and we are closed! ";}
+}
 }
